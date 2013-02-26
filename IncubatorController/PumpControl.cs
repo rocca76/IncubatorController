a06f10a3b92f193cc593f3a2819f0eb15ed2085f
rocca76 <aubin.eric@gmail.com>
2013-02-25 21:29:57 -0500

    delay change

--- IncubatorController/PumpControl.cs
+++ IncubatorController/PumpControl.cs
@@ -8,7 +8,7 @@ namespace NetduinoPlus.Controler
     class PumpControl
     {
         const int RUNING_DURATION = 1;   // seconds
-        const int WAITING_DURATION = 300; // 5 minutes
+        const int WAITING_DURATION = 180; // 3 minutes
 
         public enum PumpStateEnum
         {
