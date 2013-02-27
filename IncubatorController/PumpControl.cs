f1fe0b5ddecab9b59581b4421b365c584dbea50a
eric.aubin <eric.aubin@olympusndt.com>
2013-02-27 13:51:10 -0500

    Never activate the pump more then 1 time by minute

--- IncubatorController/PumpControl.cs
+++ IncubatorController/PumpControl.cs
@@ -19,6 +19,7 @@ namespace NetduinoPlus.Controler
         #region Private Variables
         private static PumpControl _pumpControl = null;
         private TimeSpan _duration = TimeSpan.Zero;
+        private DateTime _lastActivation;
         private PumpStateEnum _pumpState = PumpStateEnum.Stopped;
 
         private OutputPort outPump = new OutputPort(Pins.GPIO_PIN_D6, false);  //Pump
@@ -64,42 +65,46 @@ namespace NetduinoPlus.Controler
               double temperatureDelta = Abs( ProcessControl.GetInstance().TargetTemperature - ProcessControl.GetInstance().CurrentTemperature );
 
               if ((ProcessControl.GetInstance().CurrentRelativeHumidity < ProcessControl.GetInstance().TargetRelativeHumidity) && (temperatureDelta <= 1))
+              {
+                if (_duration == TimeSpan.Zero)
                 {
-                    if (_duration == TimeSpan.Zero)
-                    {
-                        if (_pumpState == PumpStateEnum.Stopped)
-                        {
-                            _pumpState = PumpStateEnum.Running;
-                            _duration = new TimeSpan(0, 0, RUNING_DURATION);
-                            outPump.Write(true);
-                        }
-                        else if (_pumpState == PumpStateEnum.Running)
-                        {
-                            _pumpState = PumpStateEnum.Stopped;
-                            _duration = new TimeSpan(0, 0, WAITING_DURATION);
-                            outPump.Write(false);
-                        }
-                    }
-                }
-                else
-                {
+                  TimeSpan timeDelta = DateTime.Now.Subtract(_lastActivation);
+
+                  if (_pumpState == PumpStateEnum.Stopped && timeDelta.Minutes > 1 )
+                  {
+                    _pumpState = PumpStateEnum.Running;
+                    _duration = new TimeSpan(0, 0, RUNING_DURATION);
+                    outPump.Write(true);
+
+                    _lastActivation = DateTime.Now;
+                  }
+                  else if (_pumpState == PumpStateEnum.Running)
+                  {
                     _pumpState = PumpStateEnum.Stopped;
-                    _duration = TimeSpan.Zero;
+                    _duration = new TimeSpan(0, 0, WAITING_DURATION);
                     outPump.Write(false);
+                  }
                 }
-            }
-            else
-            {
+              }
+              else
+              {
                 _pumpState = PumpStateEnum.Stopped;
                 _duration = TimeSpan.Zero;
                 outPump.Write(false);
-            }
+              }
+          }
+          else
+          {
+              _pumpState = PumpStateEnum.Stopped;
+              _duration = TimeSpan.Zero;
+              outPump.Write(false);
+          }
         }
         #endregion
 
 
         #region Private Methods
-        public static double Abs(double value)
+        private static double Abs(double value)
         {
           return (value >= 0) ? value : -value;
         }
